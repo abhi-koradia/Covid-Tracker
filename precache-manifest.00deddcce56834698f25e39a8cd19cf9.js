94673d9de26f0d2ce75a672ce81ef1fabab77648
Abhi Koradia <abhikoradia@gmail.com>
2021-08-27 17:26:38 +0530

    Updates

---
+++ precache-manifest.00deddcce56834698f25e39a8cd19cf9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7500d7ed27c680a26dd6194b85c97c55",
+    "url": "/Covid-Tracker/index.html"
+  },
+  {
+    "revision": "d9c4bba6b69942e7fb18",
+    "url": "/Covid-Tracker/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "9046fff5fd2fbac50569",
+    "url": "/Covid-Tracker/static/css/main.54d43338.chunk.css"
+  },
+  {
+    "revision": "d9c4bba6b69942e7fb18",
+    "url": "/Covid-Tracker/static/js/2.c3990831.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/Covid-Tracker/static/js/2.c3990831.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9046fff5fd2fbac50569",
+    "url": "/Covid-Tracker/static/js/main.bad21499.chunk.js"
+  },
+  {
+    "revision": "ae45445b70030a69d951",
+    "url": "/Covid-Tracker/static/js/runtime-main.d4cdb983.js"
+  }
+]);
